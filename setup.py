6dd62d0c1caf4e980414d04f83b0cb38551ec09f
lphoogenboom <hoogenboom.l.p@gmail.com>
2026-03-30 14:11:47 +0200

    adds functionality test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kegelib',
+    version='0.1.a',
+    description='A library to support retinal histology segmentation',
+    author='Laurens Paul Hoogenboom',
+    author_email='hoogenboom.l.p@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
